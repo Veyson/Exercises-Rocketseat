3e9424294fd1ec32471270e90a6ede9318654ef8
Luis Fernando Cezar dos Santos <luisfernando_cezar@hotmail.com>
2020-01-29 20:42:32 -0300

    Rocketseat - Project | Save in storage;

--- Project/todos.js
+++ Project/todos.js
@@ -2,11 +2,8 @@ var listElement = document.querySelector('#app ul');
 var inputElement = document.querySelector('#app input');
 var buttonElement = document.querySelector('#app button');
 
-var todos = [
-    'Fazer café',
-    'Estudar Javascript',
-    'Estudar comunidade da Rockeat'
-];
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos(){
     listElement.innerHTML = '';
@@ -41,6 +38,7 @@ function addTodo(){
     todos.push(todoText);
     inputElement.value = '';
     renderTodos();
+    saveToStorage();
 }
 
 buttonElement.onclick = addTodo;
@@ -48,4 +46,10 @@ buttonElement.onclick = addTodo;
 function deleteTodo(pos){
     todos.splice(pos,1);
     renderTodos();
+    saveToStorage();
+}
+
+
+function saveToStorage(){    
+    localStorage.setItem('list_todos', JSON.stringify(todos));
 }
