daef46a7e2823cb95313373d8468f6e7f68d60e6
Luis Fernando Cezar dos Santos <luisfernando_cezar@hotmail.com>
2020-01-31 19:50:36 -0300

    Rocketseat - Project | Configurando arquivo 'main.js';

--- Project-ES6/main.js
+++ Project-ES6/main.js
@@ -1,7 +1,41 @@
-alert('Teste');
+class List{
+    constructor(){
+        this.data = [];
+    }
 
-class Teste{
-    metodo(){
-        
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List{
+    constructor(){
+        super();
+
+        this.usuario = 'Fernando';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+
+//////////////Metodos Estaticos/////////////////////
+
+class Matematica{
+    static soma(a,b){
+        return a + b;
+    }
+}
+
+
+console.log(Matematica.soma(1,2));
